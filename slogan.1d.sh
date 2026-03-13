6b393f2c4a063553525f06c82c92349476d9250d
liumiao36 <liumiao36@xdf.cn>
2026-03-13 15:06:19 +0800

    feat:version1.0.0

---
+++ slogan.1d.sh
@@ -0,0 +1,4 @@
+echo " = Stay Curious + Stay Rational | sfimage=function"
+echo "---"
+echo "First Principles Thinking > | href=https://en.wikipedia.org/wiki/First_principles"
+echo "Daily Reflection | shell=open terminal"
